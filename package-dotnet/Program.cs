5b8b9b56a2238dee7ffb10f903c977f93c3123c6
mrdaz1992 <darrenpick2000@yahoo.co.uk>
2024-09-15 20:05:15 +0000

    create an azure resource group using the C# Azure SDK

---
+++ package-dotnet/Program.cs
@@ -0,0 +1,33 @@
+﻿using Azure;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.Storage;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main (string[] args)
+    {
+        // string subscriptionId = "c8ec073b-e265-49d2-95d2-65ee82096ecf";
+        string resourceGroupName = "my-sdk-dp";
+        AzureLocation location = AzureLocation.UKWest;
+
+        // Authenticate with Azure
+        var credential = new DefaultAzureCredential();
+
+        // Initialize Azure ARM Client
+        var armClient = new ArmClient(credential);
+
+        // Create or get access to the resource group
+        SubscriptionResource subscription = await armClient.GetDefaultSubscriptionAsync();
+        ResourceGroupCollection resourceGroups = subscription.GetResourceGroups();
+        ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+        ArmOperation<ResourceGroupResource> operation = await resourceGroups.CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+        ResourceGroupResource resourceGroup = operation.Value;
+
+        Console.WriteLine("Resource Group created successfully");
+    }
+}
